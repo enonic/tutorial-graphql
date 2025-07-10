ad23bb4e13555d8adc35515adefdeab22cdb9d3f
Anatol Sialitski <asi@enonic.com>
2025-07-10 15:33:34 +0200

    Tutorial how to build your own GraphQL API #1

--- src/main/resources/apis/graphql/schema.js
+++ src/main/resources/apis/graphql/schema.js
@@ -2,7 +2,7 @@ const graphQlLib = require('/lib/graphql');
 
 const schemaGenerator = graphQlLib.newSchemaGenerator();
 
-const storage = [];
+const storage = {};
 
 const noteType = schemaGenerator.createObjectType({
     name: 'Note',
@@ -14,66 +14,85 @@ const noteType = schemaGenerator.createObjectType({
             type: graphQlLib.nonNull(graphQlLib.GraphQLString),
         },
         content: {
-            type: graphQlLib.nonNull(graphQlLib.GraphQLString),
+            type: graphQlLib.GraphQLString,
+        },
+        createdAt: {
+            type: graphQlLib.nonNull(graphQlLib.GraphQLString)
+        }
+    }
+});
+
+const rootQueryType = schemaGenerator.createObjectType({
+    name: 'Query',
+    fields: {
+        serverTime: {
+            type: graphQlLib.GraphQLString,
+            resolve: (env) => {
+                return new Date().toISOString();
+            }
+        },
+        getNote: {
+            type: graphQlLib.reference('Note'),
+            args: {
+                id: graphQlLib.nonNull(graphQlLib.GraphQLID),
+            },
+            resolve: (env) => {
+                return storage[env.args.id];
+            },
+        },
+        getNotes: {
+            type: graphQlLib.list(graphQlLib.reference('Note')),
+            resolve: (env) => {
+                const result = [];
+                for (let key in storage) {
+                    if (storage.hasOwnProperty(key)) {
+                        result.push(storage[key]);
+                    }
+                }
+                return result;
+            }
+        }
+    },
+});
+
+const rootMutationType = schemaGenerator.createObjectType({
+    name: 'Mutation',
+    fields: {
+        createNote: {
+            type: graphQlLib.reference('Note'),
+            args: {
+                title: graphQlLib.nonNull(graphQlLib.GraphQLString),
+                content: graphQlLib.nonNull(graphQlLib.GraphQLString),
+            },
+            resolve: (env) => {
+                const note = {
+                    id: Math.random().toString(36).substring(2, 15),
+                    title: env.args.title,
+                    content: env.args.content,
+                    createdAt: new Date().toISOString(),
+                };
+                storage[note.id] = note;
+                return note;
+            }
+        },
+        deleteNote: {
+            type: graphQlLib.reference('Note'),
+            args: {
+                id: graphQlLib.nonNull(graphQlLib.GraphQLID),
+            },
+            resolve: (env) => {
+                const id = env.args.id;
+                const note = storage[id];
+                delete storage[id];
+                return note;
+            }
         }
     }
 });
 
 const graphQLSchema = schemaGenerator.createSchema({
-    query: schemaGenerator.createObjectType({
-        name: 'Query',
-        fields: {
-            serverTime: {
-                type: graphQlLib.GraphQLString,
-                resolve: (env) => {
-                    return new Date().toISOString();
-                }
-            },
-            getNote: {
-                type: graphQlLib.reference('Note'),
-                args: {
-                    id: graphQlLib.nonNull(graphQlLib.GraphQLID),
-                },
-                resolve: (env) => {
-                    let result = null;
-                    for (let i = 0; i < storage.length; i++) {
-                        if (storage[i].id === env.args.id) {
-                            result = storage[i];
-                            break;
-                        }
-                    }
-                    return result;
-                },
-            },
-            getNodes: {
-                type: graphQlLib.list(graphQlLib.reference('Note')),
-                resolve: (env) => {
-                    return storage;
-                }
-            }
-        },
-    }),
-    mutation: schemaGenerator.createObjectType({
-        name: 'Mutation',
-        fields: {
-            createNote: {
-                type: graphQlLib.reference('Note'),
-                args: {
-                    title: graphQlLib.nonNull(graphQlLib.GraphQLString),
-                    content: graphQlLib.nonNull(graphQlLib.GraphQLString),
-                },
-                resolve: (env) => {
-                    const note = {
-                        id: Math.random().toString(36).substring(2, 15),
-                        title: env.args.title,
-                        content: env.args.content,
-                    };
-                    storage.push(note);
-                    return note;
-                }
-            }
-        }
-    }),
+    query: rootQueryType,
+    mutation: rootMutationType,
     dictionary: [noteType]
 });
 
